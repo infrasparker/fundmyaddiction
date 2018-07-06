243460fd225e2b791e8c25becce08593b36152d8
Coext <scott.norton12@gmail.com>
2018-07-06 02:00:59 -0400

    winning-addition-issue

--- src/app/casino/casino.component.ts
+++ src/app/casino/casino.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { PlayerService } from '../player/player.service';
+import { Player } from '../player/player.model';
 /*
 Select heads or tails
 Enter amount to bet (numbers only)
@@ -11,10 +13,48 @@ Top Right number updated
   styleUrls: ['./casino.component.css']
 })
 export class CasinoComponent implements OnInit {
+  //@Output() onFlip = new EventEmitter;
+  player: Player;
+  private credits: number;
+  private numBet: number;
+  private heads:boolean = true;
+  private message:string;
 
-  constructor() { }
+  constructor(private playerService: PlayerService) { 
+    this.credits = playerService.player.getCredits();
+  }
+  
+  onFlipClick(amount:number,heads:boolean): void {
+    if(amount > this.credits){
+      this.message = "you don't got that kinda cash tho";
+    }
+    else{
+      this.numBet = amount;
+      this.heads = heads;
+      this.runBet();
+    }
+  }
+
+  runBet(): void { //runs bet and returns new credit value
+    let flip: number = Math.floor(Math.random() * 2); // 0  = tails , 1 = heads
+    if( (flip == 1 && this.heads) || (flip == 0 && !this.heads) ){
+      this.player.changeCredits(this.numBet);
+      this.message = "HERE COMES THE MONEY"
+    }
+    else{
+      this.player.changeCredits(-this.numBet);
+      this.message = "REEEEEEEE"
+    }
+    this.credits = this.player.getCredits();
+  }
 
   ngOnInit() {
+    this.player = this.playerService.player;
+    console.log(this.player);
+    this.playerService.updated.subscribe((resp) => {
+      this.player = resp;
+      console.log(resp);
+    })
   }
 
 }
